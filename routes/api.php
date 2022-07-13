8a145aeb6c8301ae2b664017c595a83e756de521
esraaas2018 <esraaas2018@gmail.com>
2022-07-13 10:54:54 +0300

    13-7

--- routes/api.php
+++ routes/api.php
@@ -30,35 +30,60 @@ Route::middleware(['treblle'])->group(function () {
     // YOUR API ROUTES GO HERE
     Route::post('/register', [UserController::class, 'register']);
     Route::post('/login', [UserController::class, 'login']);
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+
+        Route::get('/logout', [UserController::class, 'logout']);
+        //project
+        Route::apiResource('projects', "ProjectController");
+        Route::post('/projects/{project}/addParticipant', [ProjectController::class ,'addUser' ]);
+        Route::post('/projects/{project}/revokeParticipant', [ProjectController::class ,'revokeUser' ]);
+        //sprint
+        Route::apiResource('sprints', "SprintController")->except(['store','index']);
+        Route::post('/projects/{project}/sprints', [SprintController::class, 'store']);
+        Route::get('/projects/{project}/sprints', [SprintController::class, 'index']);
+        Route::post('/sprints/{sprint}/runSprint', [SprintController::class, 'runSprint']);
+        Route::post('/sprints/{sprint}/offSprint', [SprintController::class, 'offSprint']);
+        //task
+        Route::apiResource('tasks', "TaskController")->except(['store', 'index']);
+        //board
+        Route::get('/projects/{project}/tasks',  [TaskController::class, 'index']);
+        Route::put('/tasks/{task}/change-status',  [TaskController::class, 'changeStatus']);
+        Route::post('/sprints/{sprint}/tasks',  [TaskController::class, 'store']);
+
+        Route::get('/projects/{project}/statuses',  [StatusController::class, 'index']);
+
+        Route::put('/tasks/{task}/pin',  [TaskController::class, 'pinTask']);
+        Route::apiResource('personal_tasks', "PersonalTaskController");
+    });
 });
 
 //Route::post('/register', [UserController::class, 'register']);
 //Route::post('/login', [UserController::class, 'login']);
-Route::group(['middleware' => 'auth:sanctum'], function () {
-
-    Route::get('/logout', [UserController::class, 'logout']);
-    //project
-    Route::apiResource('projects', "ProjectController");
-    Route::post('/projects/{project}/addParticipant', [ProjectController::class ,'addUser' ]);
-    Route::post('/projects/{project}/revokeParticipant', [ProjectController::class ,'revokeUser' ]);
-    //sprint
-    Route::apiResource('sprints', "SprintController")->except(['store','index']);
-    Route::post('/projects/{project}/sprints', [SprintController::class, 'store']);
-    Route::get('/projects/{project}/sprints', [SprintController::class, 'index']);
-    Route::post('/sprints/{sprint}/runSprint', [SprintController::class, 'runSprint']);
-    Route::post('/sprints/{sprint}/offSprint', [SprintController::class, 'offSprint']);
-    //task
-    Route::apiResource('tasks', "TaskController")->except(['store', 'index']);
-    //board
-    Route::get('/projects/{project}/tasks',  [TaskController::class, 'index']);
-    Route::put('/tasks/{task}/change-status',  [TaskController::class, 'changeStatus']);
-    Route::post('/sprints/{sprint}/tasks',  [TaskController::class, 'store']);
-
-    Route::get('/projects/{project}/statuses',  [StatusController::class, 'index']);
-
-    Route::put('/tasks/{task}/pin',  [TaskController::class, 'pinTask']);
-    Route::apiResource('personal_tasks', "PersonalTaskController");
-});
+//Route::group(['middleware' => 'auth:sanctum'], function () {
+//
+//    Route::get('/logout', [UserController::class, 'logout']);
+//    //project
+//    Route::apiResource('projects', "ProjectController");
+//    Route::post('/projects/{project}/addParticipant', [ProjectController::class ,'addUser' ]);
+//    Route::post('/projects/{project}/revokeParticipant', [ProjectController::class ,'revokeUser' ]);
+//    //sprint
+//    Route::apiResource('sprints', "SprintController")->except(['store','index']);
+//    Route::post('/projects/{project}/sprints', [SprintController::class, 'store']);
+//    Route::get('/projects/{project}/sprints', [SprintController::class, 'index']);
+//    Route::post('/sprints/{sprint}/runSprint', [SprintController::class, 'runSprint']);
+//    Route::post('/sprints/{sprint}/offSprint', [SprintController::class, 'offSprint']);
+//    //task
+//    Route::apiResource('tasks', "TaskController")->except(['store', 'index']);
+//    //board
+//    Route::get('/projects/{project}/tasks',  [TaskController::class, 'index']);
+//    Route::put('/tasks/{task}/change-status',  [TaskController::class, 'changeStatus']);
+//    Route::post('/sprints/{sprint}/tasks',  [TaskController::class, 'store']);
+//
+//    Route::get('/projects/{project}/statuses',  [StatusController::class, 'index']);
+//
+//    Route::put('/tasks/{task}/pin',  [TaskController::class, 'pinTask']);
+//    Route::apiResource('personal_tasks', "PersonalTaskController");
+//});
 
 Route::get('/test', function (Request $request) {
     $project = Project::first();
